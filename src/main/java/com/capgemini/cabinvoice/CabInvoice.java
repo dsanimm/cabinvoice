d3185a4a2037ddd65d3162047e0c5c53f45e857b
dsanimm <dsanimm@gmail.com>
2020-10-20 23:24:50 +0530

    Added Ride Repository and enhanced Invoice

--- src/main/java/com/capgemini/cabinvoice/CabInvoice.java
+++ src/main/java/com/capgemini/cabinvoice/CabInvoice.java
@@ -4,19 +4,30 @@ public class CabInvoice {
 	public final int COST_PER_MIN = 1;
 	public final int COST_MIN = 5;
 	public final int COST_PER_KM = 10;
+	RideRepository rideRepository;
+
+	public CabInvoice() {
+		this.rideRepository = new RideRepository();
+	}
 
 	public double calculateFare(double distance, double time) {
 		double fare = distance * COST_PER_KM + time * COST_PER_MIN;
-		if (fare < COST_MIN)
-			return COST_MIN;
-		return fare;
+		return Math.max(fare, COST_MIN);
 	}
 
-	public double calculateFare(Ride[] rides) {
+	public InvoiceSummary calculateFare(Ride[] ride) {
 		double totalFare = 0;
-		for (Ride ride : rides)
-			totalFare += calculateFare(ride.distance, ride.timeInMinute);
-		return totalFare;
+		for (Ride rides : ride)
+			totalFare += this.calculateFare(rides.distance, rides.timeInMinute);
+		return new InvoiceSummary(ride.length, (int) totalFare);
 	}
 
+	public void addRides(String userId, Ride[] ride) {
+		rideRepository.addRide(userId, ride);
+
+	}
+
+	public InvoiceSummary getInvoiceSummary(String userId) {
+		return this.calculateFare(rideRepository.getRides(userId));
+	}
 }
--- src/test/java/com/capgemini/cabinvoice/CabInvoiceTest.java
+++ src/test/java/com/capgemini/cabinvoice/CabInvoiceTest.java
@@ -1,29 +1,39 @@
 package com.capgemini.cabinvoice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceTest {
+	CabInvoice invoiceService;
+
+	@Before
+	public void setUp() throws Exception {
+		invoiceService = new CabInvoice();
+	}
 
 	@Test
 	public void givenDistanceAndTime_ShouldReturnCorrectTotalFare() {
-		CabInvoice invoiceService = new CabInvoice();
 		double fare = invoiceService.calculateFare(2.0, 5);
 		Assert.assertEquals(25, fare, 0.0);
 	}
 
 	@Test
 	public void givenMinDistanceAndTime_ShouldReturnCorrectTotalFare() {
-		CabInvoice invoiceService = new CabInvoice();
-		double fare = invoiceService.calculateFare(0.5, 3);
-		Assert.assertEquals(8, fare, 0.0);
+		Ride[] ride = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		InvoiceSummary invoiceSummary = invoiceService.calculateFare(ride);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
 	}
 
 	@Test
-	public void givenMultipleRidesShouldReturnTotalFare() {
-		CabInvoice invoiceService = new CabInvoice();
-		Ride[] rides = { new Ride(2.5, 5), new Ride(0.2, 1) };
-		double fare = invoiceService.calculateFare(rides);
-		Assert.assertEquals(35, fare, 0);
+	public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
+		String userId = "plk@ggg.com";
+		Ride[] ride = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		invoiceService.addRides(userId, ride);
+		InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary(userId);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+
 	}
 }
